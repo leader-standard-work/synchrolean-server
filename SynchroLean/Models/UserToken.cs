0243c9218847aba2ff63f548158aa837f67e07df
Barend Venter <barendventer@gmail.com>
2018-07-01 02:15:12 -0700

    Pull changes from development

--- SynchroLean/Models/UserToken.cs
+++ SynchroLean/Models/UserToken.cs
@@ -85,5 +85,24 @@ namespace SynchroLean.Models
             // and is from one of our trusted providers...
             return validRightNow;
         }
+
+        IEnumerable<DayOfWeek> convertDaysOfWeekFromBits(sbyte bits)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if ((bits & 1) > 0) yield return (DayOfWeek)i;
+                bits >>= 1;
+            }
+        }
+
+        sbyte convertDaysOfWeekToBits(IEnumerable<DayOfWeek> days)
+        {
+            sbyte result = 0;
+            foreach(DayOfWeek weekday in days)
+            {
+                result |= (sbyte)(1 << (sbyte)(weekday));
+            }
+            return result;
+        }
     }
 }
