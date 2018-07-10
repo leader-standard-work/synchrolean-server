6bc848d0c771d5a1faa3dd7275ff2a7f6f0450ea
Barend Venter <barendventer@gmail.com>
2018-07-10 15:43:05 -0700

    Interface for the invitation repository

---
+++ SynchroLean/Core/IAddUserRequestRepository.cs
@@ -0,0 +1,19 @@
+﻿using SynchroLean.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SynchroLean.Core
+{
+    interface IAddUserRequestRepository
+    {
+        Task AddAsync(AddUserRequest request);
+        Task<AddUserRequest> GetAddUserRequestAsync(int addUserRequestId);
+        Task<IEnumerable<AddUserRequest>> GetAddUserRequestsAsync();
+        Task<IEnumerable<AddUserRequest>> GetAddUserRequestsPendingApprovalAsync(int ownerId);
+        Task<IEnumerable<AddUserRequest>> GetAddUserRequestsPendingAcceptanceAsync(int ownerId);
+        Task<Boolean> AddUserRequestExists(int addUserRequestId);
+        Task DeleteAddUserRequestAsync(int addUserRequestId);
+    }
+}
