f250a6a4eaf378e73217f75e288458bf73b706c1
Barend Venter <barendventer@gmail.com>
2018-07-13 19:17:59 -0700

    New model for team permissions

--- SynchroLean/Persistence/SynchroLeanDbContext.cs
+++ SynchroLean/Persistence/SynchroLeanDbContext.cs
@@ -9,10 +9,15 @@ namespace SynchroLean.Persistence
         public DbSet<Team> Teams { get; set; }
         public DbSet<UserAccount> UserAccounts { get; set; }
         public DbSet<AddUserRequest> AddUserRequests { get; set; }
-
+        public DbSet<TeamPermission> TeamPermissions { get; set; }
         public SynchroLeanDbContext(DbContextOptions<SynchroLeanDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeamPermission>().HasKey(perm => new { perm.SubjectTeamId, perm.ObjectTeamId });
         }
     }
 }
