5f594bf32702354e45aa2f23d10f58cc53f2026f
Cole Phares <cophares@pdx.edu>
2018-07-15 21:16:51 -0700

    fixed conflict

--- SynchroLean/Controllers/AccountsController.cs
+++ SynchroLean/Controllers/AccountsController.cs
@@ -42,6 +42,7 @@ namespace SynchroLean.Controllers
             // Map account resource to model
             var account = new UserAccount 
             {
+                //OwnerId = userAccountResource.OwnerId
                 TeamId = userAccountResource.TeamId,
                 FirstName = userAccountResource.FirstName,
                 LastName = userAccountResource.LastName,
