b2ddd69f1116e049b1246e13c00c6bc5c1e71860
Cole Phares <cophares@pdx.edu>
2018-08-06 12:28:24 -0700

    fixed confliects

--- SynchroLean/Persistence/TodoRepository.cs
+++ SynchroLean/Persistence/TodoRepository.cs
@@ -137,23 +137,6 @@ namespace SynchroLean.Persistence
             }
         }
 
-<<<<<<< HEAD
-        public async Task RefreshTodo(int taskId)
-        {
-            var task = context.UserTasks.Find(taskId);
-            if (task == null) return; //invalid, nothing to do
-            var todo = await context.Todos.Where(td => td.TaskId == taskId).FirstOrDefaultAsync();
-            DateTime? todoCompletion = null;
-            if (todo != null)
-            {
-                todoCompletion = todo.Completed;
-                context.Todos.Remove(todo);
-            }
-            await this.AddTodoAsync(taskId);
-            var newTodo = await context.Todos.Where(td => td.TaskId == taskId).FirstOrDefaultAsync();
-            if (newTodo != null) newTodo.Completed = todoCompletion;
-        }
-=======
         public async Task RefreshTodo(int taskId) 
         { 
             var task = context.UserTasks.Find(taskId); 
@@ -169,6 +152,5 @@ namespace SynchroLean.Persistence
             var newTodo = await context.Todos.Where(td => td.TaskId == taskId).FirstOrDefaultAsync(); 
             if (newTodo != null) newTodo.Completed = todoCompletion; 
         } 
->>>>>>> 1aff63967e64ee9a89dbdf87cb07d3aee78797e2
     }
 }
