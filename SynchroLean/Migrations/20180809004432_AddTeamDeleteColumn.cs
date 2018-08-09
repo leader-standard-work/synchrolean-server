a9843bfc8c90da20f3853e358739872771bdf6c7
Barend Venter <barendventer@gmail.com>
2018-08-08 17:45:15 -0700

    Missing migration

---
+++ SynchroLean/Migrations/20180809004432_AddTeamDeleteColumn.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SynchroLean.Migrations
+{
+    public partial class AddTeamDeleteColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Todos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deleted",
+                table: "Teams",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_TeamId",
+                table: "Todos",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Teams_TeamId",
+                table: "Todos",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Teams_TeamId",
+                table: "Todos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_TeamId",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Teams");
+        }
+    }
+}
