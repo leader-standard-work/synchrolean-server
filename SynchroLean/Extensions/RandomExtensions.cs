f4763cfc1dfe62ab5c579c3fd92ec80aaedc7d32
Barend Venter <barendventer@gmail.com>
2018-08-13 23:46:02 -0700

    Extension method for randomly sampling a collection

---
+++ SynchroLean/Extensions/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SynchroLean.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static T SampleFrom<T>(this Random rng, IEnumerable<T> possibilities)
+        {
+            var outcome = rng.NextDouble() * possibilities.Count();
+            return possibilities.ElementAt((int)Math.Floor(outcome));
+        }
+    }
+}
