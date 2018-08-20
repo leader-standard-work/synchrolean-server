bb54083cdc9912f9b6579ada526862f1cf480035
Unknown <goddard2@pdx.edu>
2018-08-19 19:04:09 -0700

    Switch back to the default port

--- SynchroLean/Program.cs
+++ SynchroLean/Program.cs
@@ -20,7 +20,6 @@ namespace SynchroLean
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:80/")
                 .Build();
     }
 }
