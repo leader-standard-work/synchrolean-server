8be7dbbaf452849f6fa60b8af2fd81ec5893690b
Barend Venter <barendventer@gmail.com>
2018-08-31 12:36:25 -0700

    Remove useless variable declarations

--- SynchroLean/Rollover.cs
+++ SynchroLean/Rollover.cs
@@ -21,13 +21,6 @@ namespace SynchroLean
                 var context = scope.ServiceProvider.GetService<SynchroLeanDbContext>();
                 var unitOfWork = scope.ServiceProvider.GetService<IUnitOfWork>();
 
-                //Determine important times
-                var tomorrow = DateTime.Today + TimeSpan.FromDays(1);
-                var periodToNextMidnight = tomorrow - DateTime.Now;
-                var endOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddDays(-1);
-                var endOfWeek = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)
-                    .AddDays((int)(DayOfWeek.Saturday) - (int)(DateTime.Now.DayOfWeek) + 1);
-
                 //Clean up the to-do list for the night
                 await unitOfWork.TodoRepository.Clean();
                 Task.WaitAll(unitOfWork.CompleteAsync());
